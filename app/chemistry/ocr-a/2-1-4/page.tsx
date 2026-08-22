ad65db0f70fa3af21e657988ed55e8ad376d242a
s19-netizen <sawdaj19@gmail.com>
2026-08-22 16:48:25 +0100

    Connect 2.1.4 placeholder page

---
+++ app/chemistry/ocr-a/2-1-4/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderSpec } from "@/components/PlaceholderSpec";
+
+export default function AcidsPage() {
+  return <PlaceholderSpec code="2.1.4" title="Acids" previous="/chemistry/ocr-a/2-1-3" next="/chemistry/ocr-a/2-1-5" />;
+}
