b5547b99e7701713c92ea5ba087bdf0dd9b036c0
s19-netizen <sawdaj19@gmail.com>
2026-08-22 16:48:32 +0100

    Connect 2.1.5 placeholder page

---
+++ app/chemistry/ocr-a/2-1-5/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderSpec } from "@/components/PlaceholderSpec";
+
+export default function RedoxPage() {
+  return <PlaceholderSpec code="2.1.5" title="Redox" previous="/chemistry/ocr-a/2-1-4" />;
+}
