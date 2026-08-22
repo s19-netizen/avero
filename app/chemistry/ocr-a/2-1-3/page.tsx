2c863349254b2825855f368bfe349b99c69c9aa8
s19-netizen <sawdaj19@gmail.com>
2026-08-22 16:48:17 +0100

    Connect 2.1.3 placeholder page

---
+++ app/chemistry/ocr-a/2-1-3/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderSpec } from "@/components/PlaceholderSpec";
+
+export default function AmountOfSubstancePage() {
+  return <PlaceholderSpec code="2.1.3" title="Amount of substance" previous="/chemistry/ocr-a/2-1-2" next="/chemistry/ocr-a/2-1-4" />;
+}
