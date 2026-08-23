5690360827fe3d9f0f8f68afd7c83986e12d513f
s19-netizen <sawdaj19@gmail.com>
2026-08-23 12:41:15 +0100

    Use custom amount of substance study page

--- app/chemistry/ocr-a/2-1-3/page.tsx
+++ app/chemistry/ocr-a/2-1-3/page.tsx
@@ -1,5 +1,5 @@
-import { PlaceholderSpec } from "@/components/PlaceholderSpec";
+import { Module2TopicPage } from "@/components/Module2TopicPage";
 
 export default function AmountOfSubstancePage() {
-  return <PlaceholderSpec code="2.1.3" title="Amount of substance" previous="/chemistry/ocr-a/2-1-2" next="/chemistry/ocr-a/2-1-4" />;
+  return <Module2TopicPage code="2.1.3" title="Amount of substance" topic="moles" previous="/chemistry/ocr-a/2-1-2" next="/chemistry/ocr-a/2-1-4" />;
 }
