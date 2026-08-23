d933a6991886670d262023c3e0db05753a889c88
s19-netizen <sawdaj19@gmail.com>
2026-08-23 12:15:59 +0100

    Add bonding and structure prototype page

---
+++ app/chemistry/ocr-a/2-2-2/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderSpec } from "@/components/PlaceholderSpec";
+
+export default function BondingStructurePage() {
+  return <PlaceholderSpec code="2.2.2" title="Bonding and structure" previous="/chemistry/ocr-a/2-2-1" />;
+}
