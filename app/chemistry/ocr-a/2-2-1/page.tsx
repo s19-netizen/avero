452e6de163aec705b47266c634217c76c4841f3d
s19-netizen <sawdaj19@gmail.com>
2026-08-23 12:15:53 +0100

    Add electron structure prototype page

---
+++ app/chemistry/ocr-a/2-2-1/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderSpec } from "@/components/PlaceholderSpec";
+
+export default function ElectronStructurePage() {
+  return <PlaceholderSpec code="2.2.1" title="Electron structure" previous="/chemistry/ocr-a/2-1-5" next="/chemistry/ocr-a/2-2-2" />;
+}
