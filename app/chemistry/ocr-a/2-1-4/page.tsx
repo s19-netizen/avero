90c49b454e2b28e046ae37b2ad9ac3d4fa1adcdf
s19-netizen <sawdaj19@gmail.com>
2026-08-23 14:51:10 +0100

    Turn Acids into topic overview

--- app/chemistry/ocr-a/2-1-4/page.tsx
+++ app/chemistry/ocr-a/2-1-4/page.tsx
@@ -1,5 +1,7 @@
-import { Module2TopicPage } from "@/components/Module2TopicPage";
+import { TopicOverview } from "@/components/TopicOverview";
+import { getModule2Topic } from "@/lib/module2TopicData";
 
 export default function AcidsPage() {
-  return <Module2TopicPage code="2.1.4" title="Acids" topic="acids" previous="/chemistry/ocr-a/2-1-3" next="/chemistry/ocr-a/2-1-5" />;
+  const topic = getModule2Topic("2.1.4");
+  return <TopicOverview topic={topic} />;
 }
