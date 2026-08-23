8d83088cb4e2ce53cf945dc616517af8972e48b0
s19-netizen <sawdaj19@gmail.com>
2026-08-23 14:51:00 +0100

    Turn Amount of substance into topic overview

--- app/chemistry/ocr-a/2-1-3/page.tsx
+++ app/chemistry/ocr-a/2-1-3/page.tsx
@@ -1,5 +1,7 @@
-import { Module2TopicPage } from "@/components/Module2TopicPage";
+import { TopicOverview } from "@/components/TopicOverview";
+import { getModule2Topic } from "@/lib/module2TopicData";
 
 export default function AmountOfSubstancePage() {
-  return <Module2TopicPage code="2.1.3" title="Amount of substance" topic="moles" previous="/chemistry/ocr-a/2-1-2" next="/chemistry/ocr-a/2-1-4" />;
+  const topic = getModule2Topic("2.1.3");
+  return <TopicOverview topic={topic} />;
 }
