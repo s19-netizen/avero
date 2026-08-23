fb8ab15b66546d4ed0a3fbd8e6542d8cd9b96734
s19-netizen <sawdaj19@gmail.com>
2026-08-23 12:16:29 +0100

    Connect redox to electron structure

--- app/chemistry/ocr-a/2-1-5/page.tsx
+++ app/chemistry/ocr-a/2-1-5/page.tsx
@@ -1,5 +1,5 @@
 import { PlaceholderSpec } from "@/components/PlaceholderSpec";
 
 export default function RedoxPage() {
-  return <PlaceholderSpec code="2.1.5" title="Redox" previous="/chemistry/ocr-a/2-1-4" />;
+  return <PlaceholderSpec code="2.1.5" title="Redox" previous="/chemistry/ocr-a/2-1-4" next="/chemistry/ocr-a/2-2-1" />;
 }
